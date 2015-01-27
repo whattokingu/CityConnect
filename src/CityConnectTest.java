8dabe5a60785379e41bb128722b2217bf01af634
whattokingu <keep.it.that.way@gmail.com>
2015-01-27 13:06:19 +0800

    Added 2 more tests

--- src/CityConnectTest.java
+++ src/CityConnectTest.java
@@ -9,11 +9,21 @@ import org.junit.Test;
 public class CityConnectTest {
 
 	@Test
-	public void invalidCommandTest() {
+	public void testInvalidCommand() {
 		String input="abcd";
-		initialise(input);
-		assertEquals("invalid command format :abcd",CityConnect.executeCommand(input));
 		
+		assertEquals("invalid command format :abcd",CityConnect.executeCommand(input));	
+	}
+	public void testEmptyCommand() {
+		String input="";
+		
+		assertEquals("invalid command format :",CityConnect.executeCommand(input));	
+	}
+	
+	public void testUnaddedRoute(){
+		String input="getdistance aaa bbb";
+		
+		assertEquals("No route exists from aaa to bbb!",CityConnect.executeCommand(input));	
 	}
 	
 	//method to simulate command line input by changing System.in to take in strings
